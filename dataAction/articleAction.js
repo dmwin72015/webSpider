120ca9babd620ee42607d37db404c38f6d347b65
marourou2016@icloud.com <marourou2016@icloud.com>
2016-07-07 22:07:11 +0800

    重新调整了一下结构

---
+++ dataAction/articleAction.js
@@ -0,0 +1,84 @@
+/**
+ * Created by mjj on 16/7/7.
+ */
+
+const headrs = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+};
+
+/**
+ * 获取所有文章
+ * @param {JSON} options - 获取数据传递的参数
+ * @param {function} callback - 回掉函数 传递参数(rows) ,rows: 返回查询结果集合.
+ * */
+function getAll(options, callback) {
+    if (typeof options === 'function') {
+        callback = options;
+        options = undefined;
+    }
+    var sql = 'SELECT * FROM `blog_article` WHERE arti_name !="" order by pub_time';
+    mysqlTool.query(sql, function(err, rows) {
+        callback && callback(err, rows);
+    });
+}
+/**
+ * 获取范围从start开始到end结束的数据,
+ * 如果不传递start,end.查询出所有结果.只传递了其中一个参数,则只查询前n条记录
+ * @param {number} [start] - 开始的记录数
+ * @param {number} [end] - 结束的记录数
+ * @param {function} callback - 回掉函数
+ * */
+function getLimit(start, end, callback) {
+    var len = arguments.length;
+    switch (len) {
+        case 1:
+            callback = start;
+            getAll(callback);
+            break;
+        case 2:
+            if (typeof start != 'number') return;
+            callback = end;
+            start = 0;
+            end = start < 0 ? 0 : start;
+            break;
+        default:
+            if (typeof start != 'number' || typeof end != 'number') {
+                return;
+            }
+            if (start < 0 || end < 0) {
+                start = 0;
+                end = 0;
+            }
+            if (start < end) {
+                return;
+            }
+            break;
+    }
+    var sql = 'SELECT * FROM `blog_article` limit ' + start + ',' + end + ';';
+    mysqlTool.query(sql, function(err, rows) {
+        callback && callback(err, rows);
+    });
+}
+/**
+ * 根据ID获取文章
+ * @param {string|number} id - 文章ID
+ * @param {function} [callback] - 回掉函数.
+ * */
+function getById(id, callback) {
+    "use strict";
+    if (!id) return;
+    var sql = 'SELECT * FROM `blog_article` WHERE arti_name !="" and id=?';
+    baseQuery(sql, [id], callback);
+}
+
+/**
+ * 根据name获取文章
+ * @param {string} name - 文章name
+ * @param {function} [callback] - 回掉函数
+ * */
+function getByName(name, callback) {
+    "use strict";
+    if (!name) return;
+    var sql = 'SELECT * FROM `blog_article` WHERE arti_name like ?';
+    baseQuery(sql, [name], callback);
+}
--- model/article.js
+++ model/article.js
@@ -6,15 +6,12 @@ const baseQuery = require('../util/mysql_connect')['query'];
 const myEmit = require('../util/myEmitter')['emitter'];
 const addEvent = require('../util/myEmitter').addEvent;
 
-const headrs = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-};
-
 const keys = ['arti_name', 'arti_author_id', 'arti_author_name', 'arti_textcontent', 'arti_htmlcontent', 'arti_sorce', 'arti_status', 'arti_label', 'arti_cate_id', 'arti_cate_name', 'arti_editor', 'arti_from', 'pub_time', 'last_edit_time', 'read_permission', 'read_num', 'like_num', 'unlike_num', 'hot', 'create_time'];
 
 /**
  * 构造函数
  * @constructor {array} - arr 保存着与keys一一对应的数据
+ * @return  this - 生成的当前对象
  * */
 function Article(data) {
     var that = this;
@@ -30,9 +27,13 @@ function Article(data) {
     }
     return that;
 }
+/**
+ * 扩展原型,增加公用方法.
+ * 1.save @param {function} cb - 保存之后执行的回调函数
+ * */
 Article.fn = Article.prototype = {
     constructor: Article,
-    save: function(cb) {
+    save: function (cb) {
         if (!this.arti_name) return 'article_name 不存在';
         var sql = "INSERT INTO `blog_article` (`arti_name`, `arti_author_id`, `arti_author_name`, `arti_textcontent`, `arti_htmlcontent`, `arti_sorce`, `arti_status`, `arti_label`, `arti_cate_id`, `arti_cate_name`,`arti_editor` ,`arti_from`, `pub_time`, `last_edit_time`,`read_permission`,`read_num`,`like_num`,`unlike_num`,`hot`,`create_time`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?," + (this.create_time || 'now()') + ");";
         var arrData = [
@@ -60,8 +61,8 @@ Article.fn = Article.prototype = {
 
         baseQuery(sql, arrData, (err, rows) => {
             //留着测试.链接数据库出错的时候
-            if(err){
-                throw err ;
+            if (err) {
+                throw err;
                 return;
             }
             cb && cb(err, rows);
@@ -70,92 +71,17 @@ Article.fn = Article.prototype = {
 }
 
 /**
- * 获取所有文章
- * @param {JSON} options - 获取数据传递的参数
- * @param {function} callback - 回掉函数 传递参数(rows) ,rows: 返回查询结果集合.
- * */
-function getAll(options, callback) {
-    if (typeof options === 'function') {
-        callback = options;
-        options = undefined;
-    }
-    var sql = 'SELECT * FROM `blog_article` WHERE arti_name !="" order by pub_time';
-    mysqlTool.query(sql, function(err, rows) {
-        callback && callback(err, rows);
-    });
-}
-/**
- * 获取范围从start开始到end结束的数据,
- * 如果不传递start,end.查询出所有结果.只传递了其中一个参数,则只查询前n条记录
- * @param {number} [start] - 开始的记录数
- * @param {number} [end] - 结束的记录数
- * @param {function} callback - 回掉函数
- * */
-function getLimit(start, end, callback) {
-    var len = arguments.length;
-    switch (len) {
-        case 1:
-            callback = start;
-            getAll(callback);
-            break;
-        case 2:
-            if (typeof start != 'number') return;
-            callback = end;
-            start = 0;
-            end = start < 0 ? 0 : start;
-            break;
-        default:
-            if (typeof start != 'number' || typeof end != 'number') {
-                return;
-            }
-            if (start < 0 || end < 0) {
-                start = 0;
-                end = 0;
-            }
-            if (start < end) {
-                return;
-            }
-            break;
-    }
-    var sql = 'SELECT * FROM `blog_article` limit ' + start + ',' + end + ';';
-    mysqlTool.query(sql, function(err, rows) {
-        callback && callback(err, rows);
-    });
-}
-/**
- * 根据ID获取文章
- * @param {string|number} id - 文章ID
- * @param {function} [callback] - 回掉函数.
- * */
-function getById(id, callback) {
-    "use strict";
-    if (!id) return;
-    var sql = 'SELECT * FROM `blog_article` WHERE arti_name !="" and id=?';
-    baseQuery(sql, [id], callback);
-}
-
-/**
- * 根据name获取文章
- * @param {string} name - 文章name
- * @param {function} [callback] - 回掉函数
- * */
-function getByName(name, callback) {
-    "use strict";
-    if (!name) return;
-    var sql = 'SELECT * FROM `blog_article` WHERE arti_name like ?';
-    baseQuery(sql, [name], callback);
-}
-
-/**
- * 捕获取页面中文章链接
+ * 捕获取页面中文章链接,把获取到所有的符合条件的连接在返回到前台
+ * @param {string} url - 获取连接的主页面的url
+ * @param {function} cb - 回调处理函数
  * */
 function getArticleLinkFromWeb(url, cb) {
     agent.getDom(url, (err, data) => {
         if (err) {
             var json = {err: 0, d: data};
         } else {
-            var $ = data;
-            var $li = $('#alpha-inner li.module-list-item'),
+            var $ = data,
+                $li = $('#alpha-inner li.module-list-item'),
                 i = 0,
                 len = $li.length,
                 tmpArr = [];
@@ -177,17 +103,23 @@ function getArticleLinkFromWeb(url, cb) {
 }
 
 
-//获取DOM从页面中
+/**
+ * 添加监听: 获取DOM从页面中
+ * 传递过来的参数:
+ *  id(临时生成的ID,方便在后台处理之后,回传来获取前台元素),
+ *  url(当前页面的url),
+ *  socket(当前连接的socket)
+ * */
 addEvent('get article from web', (arg) => {
     if (!arg.url) return;
     var url = encodeURI(arg.url);
-    agent.getDom(url, (err, dom) => {
+    agent.getDom(url, (err, data) => {
         if (err) {
             arg.socket.emit('dataFromServer', {
                 err: 0,
-                data: dom,
+                data: data,
                 id: arg.id,
-                href: arg.href
+                href: url
             });
         } else {
             myEmit.emit('save article to database', dom, arg);
@@ -195,7 +127,10 @@ addEvent('get article from web', (arg) => {
     });
 });
 
-//保存到数据库
+/**
+ * 保存到数据库
+ * 根据传递过来的dom,以及一些其他信息:如url,socket,等等,将数据保存到数据库.
+ * */
 addEvent('save article to database', (dom, arg) => {
     var $ = dom;
     var $content = $('#content');
@@ -212,23 +147,26 @@ addEvent('save article to database', (dom, arg) => {
         arti_cate_name: $content.find('div.entry-categories ul').text(),
         arti_editor: $content.find('article.hentry p.vcard.author a').text(),
         arti_from: arg.url,
-        pub_time: $content.find('abbr.published').attr('title').replace('T',' ').split('+')[0]
+        pub_time: $content.find('abbr.published').attr('title').replace('T', ' ').split('+')[0]
     };
 
     var arti = new Article(article);
 
     arti.save((err, rows) => {
-        var oBack = err ? { err: 0, data: err } : { err: 1, data: arti };
-        oBack.url = arg.url;
-        oBack.id = arg.id;
-        oBack.rows = rows;
-        arg.socket.emit('dataFromServer', oBack);
+        var oDataBack = {
+            err: err ? 0 : 1,
+            data: err ? err : arti,
+            url : arg.url,
+            id: arg.id,
+            rows:rows
+        }
+        arg.socket.emit('dataFromServer', oDataBack);
     });
     arti = null;
 });
 
 /**
- * 使用socket的方式
+ * 使用socket的方式,监听连接事件.
  */
 
 io.on('connection', (socket) => {
@@ -245,7 +183,7 @@ io.on('connection', (socket) => {
         });
     });
 
-    
+
 });
 
 module.exports = {
