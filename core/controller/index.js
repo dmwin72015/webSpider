b4e0f5d8a7a602b82688dadd75f4b5d5431d7ffb
marourou2016@icloud.com <marourou2016@icloud.com>
2016-07-23 23:32:19 +0800

    修正上一篇，下一篇的bug

--- core/controller/index.js
+++ core/controller/index.js
@@ -17,9 +17,11 @@ module.exports = {
     },
     'article/:artid': function (req, res, next) {
         var id = req.params.artid;
-        if (!id) {
-            next();
-            return;
+        if (!id || isNaN(Number(id))) {
+            next({message:'页面不存在',
+                status:404,
+                stack:'文章的ID格式不正确'
+            });
         }
         var sql = '(SELECT * FROM blog_article WHERE id<? ORDER BY id desc limit 1) UNION ALL (SELECT * FROM blog_article WHERE id >=? ORDER BY id ASC LIMIT 2);'
         // var sql = 'SELECT id, arti_name, arti_author_name, arti_textcontent, arti_htmlcontent, arti_label, arti_cate_name, pub_time, read_num FROM blog_article WHERE id=? or id=? or id= ?';
@@ -32,8 +34,8 @@ module.exports = {
         }).catch((err)=> {
             next();
         }).then((result)=> {
-            // console.log(result);
-            if (result) {
+            console.log(result[0].arti_name);
+            if (result && result.length) {
                 if (result.length == 3) {
                     var sHtml = result[1].arti_htmlcontent;
                     res.render('blog/article', {
@@ -48,53 +50,56 @@ module.exports = {
                         prev: {name:result[0].arti_name,id:result[0].ID}
                     });
                 }else if(result.length == 2){
-                    var tempId = -1 ;
+                    var tempIndex = -1 ;
                     for(var i=0;i<2;i++){
                         if(result[i].ID == id){
-                            tempId = id;
+                            tempIndex = i;
                             break;
                         }
                     }
-                    if(tempId == -1){
+                    if(tempIndex == -1){
                         next(404);
                     }else{
-                        if(result[tempId+1]){
-                            var next = {name:result[tempId+1].arti_name,id:result[tempId+1].ID},
-                                prev = null;
+                        if(result[tempIndex+1]){
+                            var nextArt = {name:result[tempIndex+1].arti_name,id:result[tempIndex+1].ID},
+                                prevArt = null;
                         }else{
-                            var next = null,
-                                prev = {name:result[tempId-1].arti_name,id:result[tempId-1].ID};
+                            var nextArt = null,
+                                prevArt = {name:result[tempIndex-1].arti_name,id:result[tempIndex-1].ID};
                         }
                         res.render('blog/article', {
-                            title: result[tempId].arti_name,
-                            author: result[tempId].arti_author_name,
+                            title: result[tempIndex].arti_name,
+                            author: result[tempIndex].arti_author_name,
                             html: sHtml,
-                            pubtime: result[tempId].pub_time.split(' ')[0],
-                            readnum: result[tempId].read_num,
-                            text: result[tempId].arti_textcontent,
-                            categary: result[tempId].arti_cate_name.toUpperCase(),
-                            next: next,
-                            prev: prev
+                            pubtime: result[tempIndex].pub_time.split(' ')[0],
+                            readnum: result[tempIndex].read_num,
+                            text: result[tempIndex].arti_textcontent,
+                            categary: result[tempIndex].arti_cate_name.toUpperCase(),
+                            next: nextArt,
+                            prev: prevArt
                         });
                     }
                 }else if(result.length == 1){
                     if(result[0].ID == id){
                         res.render('blog/article', {
-                            title: result[1].arti_name,
-                            author: result[1].arti_author_name,
+                            title: result[0].arti_name,
+                            author: result[0].arti_author_name,
                             html: sHtml,
-                            pubtime: result[1].pub_time.split(' ')[0],
-                            readnum: result[1].read_num,
-                            text: result[1].arti_textcontent,
-                            categary: result[1].arti_cate_name.toUpperCase(),
+                            pubtime: result[0].pub_time.split(' ')[0],
+                            readnum: result[0].read_num,
+                            text: result[0].arti_textcontent,
+                            categary: result[0].arti_cate_name.toUpperCase(),
                             next: null,
                             prev: null
                         });
                     }else{
-                        next(404);
+                        next({message:'页面不存在',
+                            status:404,
+                            stack:'文章的ID不存在'
+                        });
                     }
                 }else{
-                    next(404);
+                    next();
                 }
             }
         });
